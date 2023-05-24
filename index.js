dc56a831793e8f2308548ab9c79d39c6e72b217e
Satnam Singh <satnamsingh85611@gmail.com>
2023-05-24 13:41:25 +0530

    404 error handling

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 const authMiddleware = require('./authMiddleware');
 
-
+/* Api routes */
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.get('/api/protected', authMiddleware, (req, res) => {
     const userId = req.userId;
     return res.json({ message: 'Access granted to protected route.', userId });
 });
+
+
+/* Error Middleware  */
+app.get('*', function (req, res) {
+    res.status(404).json({ error: 'Api not found' });
+});
+
 
 
 // Start the server
